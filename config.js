91c624edf168ad995ab7d081771a29f0e9a4eb8e
Tiago Morais <tiagomorais@Mac.Home>
2026-04-10 17:45:59 +0100

    Set GA4 measurement ID

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.VC_CONFIG = {
   FORMSPREE_ENDPOINT: "https://formspree.io/f/xpqoglbb",
-  GA4_MEASUREMENT_ID: "G-XXXXXXXXXX",
+  GA4_MEASUREMENT_ID: "G-4W8TDY8KT5",
   CALENDLY_URL: "https://calendly.com/tiago-morais-1/30min?back=1",
   CALENDLY_EVENT_NAME: "book_call_click"
 };
